4ebc64f4cf1f11bc016c5ca6a9d109e9adb4b01a
Igor Dal Picolo <igordalpicolo11@hotmail.com>
2023-12-28 20:10:18 -0300

    Adicionando esqueleto do projeto

---
+++ gerenciador-agenda/src/main/java/br/com/gerenciadoragenda/gerenciadoragenda/GerenciadorAgendaApplication.java
@@ -0,0 +1,13 @@
+package br.com.gerenciadoragenda.gerenciadoragenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorAgendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorAgendaApplication.class, args);
+	}
+
+}
---
+++ gerenciador-agenda/src/test/java/br/com/gerenciadoragenda/gerenciadoragenda/GerenciadorAgendaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gerenciadoragenda.gerenciadoragenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorAgendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
